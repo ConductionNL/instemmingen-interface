c3a8137ec43df78e16d312e3be5a4b7b6f9c22d5
Robert Zondervan <rjzondervan@gmail.com>
2020-03-30 11:14:32 +0200

    Tweaks on logout

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 use App\Service\CommonGroundService;
 
