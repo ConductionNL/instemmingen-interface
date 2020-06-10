282119187bf9e621f0205fd42d6eb82b15fcea5e
Robert Zondervan <Robert@conduction.nl>
2020-06-10 16:21:56 +0200

    Bijwerken met CGB

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Service\ApplicationService;
 //use App\Service\RequestService;
+use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,7 +14,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Service\CommonGroundService;
 
 /**
  * Class DeveloperController
